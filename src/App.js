595e0ed98ec4f3b5a7eaf93071f438ab9525088f
Jean <jpere547@fiu.edu>
2020-10-07 12:47:46 -0400

    removing blog components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Resumes from "./pages/Resumes";
 import Portfolios from "./pages/Portfolios";
-import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
-import BlogDetails from "./pages/BlogDetails";
 import Notfound from "./pages/Notfound";
 
 function App() {
@@ -18,8 +16,6 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/resume" component={Resumes} />
         <Route path="/portfolios" component={Portfolios} />
-        <Route path="/blogs" exact component={Blogs} />
-        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
         <Route path="/contact" component={Contact} />
         <Route path="*" component={Notfound} />
       </Switch>
